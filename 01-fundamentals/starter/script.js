176087fadadff1fcaa3ea8ced4759e3a7a48d636
eze <gmail@eze.com>
2022-10-11 15:24:16 -0300

    #2 coding challenge fundamentals1

--- 01-fundamentals/starter/script.js
+++ 01-fundamentals/starter/script.js
@@ -41,3 +41,24 @@ console.log(bmi_john, bmi_mark)
 let markHigherBMI = bmi_mark > bmi_john
 
 console.log(markHigherBMI)
+
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. 
+The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+
+if(markHigherBMI){
+    console.log(`Mark's BMI (${bmi_mark}) is higher than John's! (${bmi_john})`)
+} else {
+    console.log(`John's BMI (${bmi_john}) is higher than Mark's! (${bmi_mark}))`)
+}
+
