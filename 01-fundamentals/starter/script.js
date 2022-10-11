dd63dbc5e34018215c718c625d08de0de8925e61
eze <gmail@eze.com>
2022-10-11 13:30:46 -0300

    values and variables

--- 01-fundamentals/starter/script.js
+++ 01-fundamentals/starter/script.js
@@ -1,4 +1,12 @@
+// linking a js file
+/*
 let js = 'amazing'
 if (js === 'amazing') alert('javascript is fun!')
 
 console.log(40 + 8 + 23 - 10);
+*/
+
+// values and variables
+let firstName = "ezequiel";
+let years = 23
+console.log(firstName + ' is ' + years + ' old!')
