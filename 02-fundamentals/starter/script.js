e00cc4228221bf35e64cdb9f0465f4a9e1e87a82
eze <gmail@eze.com>
2022-10-12 22:31:09 -0300

    functions

--- 02-fundamentals/starter/script.js
+++ 02-fundamentals/starter/script.js
@@ -1,8 +1,20 @@
 // activating strict mode
 'use strict';   // algunas variables no pueden tener ciertos nombres
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if (passTest) hasDriversLicense = true;
-if(hasDriversLicense) console.log('Drivers license :D')
+if(hasDriversLicense) console.log('Drivers license :D') */
+
+// functions
+
+function fruta(apples, oranges){
+    console.log(apples, oranges);
+    const juice = `jugo con ${apples} y ${oranges}`
+    return juice;
+}
+
+const pp = fruta(5,0)
+console.log(pp)
+console.log(fruta(5,0))
