00a45fc35ae6c8e0b7f48e8fa3d3afd30c4abf73
eze <gmail@eze.com>
2022-10-12 22:21:50 -0300

    strict mode

---
+++ 02-fundamentals/starter/script.js
@@ -0,0 +1,8 @@
+// activating strict mode
+'use strict';   // algunas variables no pueden tener ciertos nombres
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if(hasDriversLicense) console.log('Drivers license :D')
