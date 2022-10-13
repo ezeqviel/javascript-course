6b50d8d3dccf56dd599b78768fdaeda63aa6d3b4
eze <slgdslvsky@gmail.com>
2022-10-13 13:20:10 -0300

    intro to objects

--- 02-fundamentals/starter/script.js
+++ 02-fundamentals/starter/script.js
@@ -144,12 +144,20 @@ GOOD LUCK 😀
 3. array tips
 4. array total
 */
-
+/*
 const calcTip = bill => bill>=50 && bill <= 300 ? bill*0.15 : bill*0.2
 const bills = [125, 555, 44]
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
 console.log(bills)
 console.log(tips)
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
-console.log(total)
+console.log(total) */
+
+// intro to objects
+
+const eze = {
+    name: 'ezequiel',
+    age: 23,
+    job: 'error 404'
+}
 
