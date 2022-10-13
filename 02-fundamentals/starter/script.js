f00c9ae23c51a29cd7387de0878228b04c451492
eze <slgdslvsky@gmail.com>
2022-10-13 14:08:14 -0300

    coding challenge #3 fundamentals2

--- 02-fundamentals/starter/script.js
+++ 02-fundamentals/starter/script.js
@@ -186,7 +186,7 @@ y su mejor amigo es ${eze.amigos[1]}`)
 */
 
 // object methods
-
+/*
 const eze = {
     name: 'ezequiel',
     age: 23,
@@ -213,3 +213,55 @@ console.log(eze['calcAge'](eze.birthYear))
 // challenge
 // "eze tiene x años y es. tiene registro."
 console.log(eze.getSummary())
+*/
+
+// Coding Challenge #3
+
+/*
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! 
+Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height 
+(Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). 
+Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. 
+Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+
+1. crear objetos con sus nombres, peso y altura
+2. calcBMI metodo. guardar en una propiedad, retornarla desde el metodo
+3. console.log
+*/
+
+const mark = {
+    name: 'mark miller',
+    peso: 78,
+    altura: 1.69,
+
+    calcBMI: function(){
+        this.bmi = this.peso / (this.altura)**2
+    }
+}
+
+const john = {
+    name: 'john smith',
+    peso: 92,
+    altura: 1.95,
+    calcBMI: function(){
+        this.bmi = this.peso / (this.altura)**2
+        return this.bmi
+    }
+}
+
+mark.calcBMI()
+john.calcBMI()
+
+if(mark.bmi > john.bmi){
+    console.log (`${mark.name} tiene mayor bmi (${mark.bmi}) que john (${john.bmi})`)
+} else {
+    console.log (`${john.name} tiene mayor bmi (${john.bmi}) que mark (${mark.bmi})`)
+}
