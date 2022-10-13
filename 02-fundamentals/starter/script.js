a9ab69038605a57947c761d252924a6e5196815d
eze <gmail@eze.com>
2022-10-12 22:35:47 -0300

    arrow functions

--- 02-fundamentals/starter/script.js
+++ 02-fundamentals/starter/script.js
@@ -8,7 +8,7 @@ if (passTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log('Drivers license :D') */
 
 // functions
-
+/*
 function fruta(apples, oranges){
     console.log(apples, oranges);
     const juice = `jugo con ${apples} y ${oranges}`
@@ -17,4 +17,18 @@ function fruta(apples, oranges){
 
 const pp = fruta(5,0)
 console.log(pp)
-console.log(fruta(5,0))
+console.log(fruta(5,0)) */
+
+// arrow functions
+
+const calcAge = birthYear => 2022 - birthYear;
+const age = calcAge(1998)
+console.log(age)
+
+const jubilacion = birthYear => {
+    const age = calcAge(birthYear)
+    const j = 65 - age;
+    return j;
+}
+
+console.log(jubilacion(1998))
