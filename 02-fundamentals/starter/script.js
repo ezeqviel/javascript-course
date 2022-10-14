77f8f4abf527a7ca260e9a314559d00ce349e1f0
eze <slgdslvsky@gmail.com>
2022-10-14 16:18:34 -0300

    fundamentals2 ✅

--- 02-fundamentals/starter/script.js
+++ 02-fundamentals/starter/script.js
@@ -309,11 +309,47 @@ for(let i = 0 ; i < eze.length ; i++){
 } */
 
 // the while loop
-
+/*
 let dado = Math.trunc(Math.random() * 6) + 1;
 
 while (dado !== 6) {
   console.log(`dado: ${dado}`);
   dado = Math.trunc(Math.random() * 6) + 1;
   if (dado === 6) console.log('ultimo');
+} */
+
+// Coding Challenge #4
+
+/*
+Let's improve Steven's tip calculator even more, this time using loops!
+
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values 
+(bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀
+
+1. bill
+*/
+const calcTip = bill => bill>=50 && bill <= 300 ? bill*0.15 : bill*0.2
+
+const bill = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = []
+const totals = []
+
+for(let i = 0 ; i < bill.length ; i++){
+    tips.push(calcTip(bill[i]))
+    totals.push(bill[i] + tips[i])
 }
+console.log(bill)
+console.log(tips, totals)
