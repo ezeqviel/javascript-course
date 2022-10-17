f0d0e360769fa601ae8ad1e02a34f516f83d7529
eze <slgdslvsky@gmail.com>
2022-10-17 16:08:14 -0300

    debug

--- 03-developerskills/starter/script.js
+++ 03-developerskills/starter/script.js
@@ -8,3 +8,24 @@
     https://nodejs.org/en/download/
     
 */
+
+// tips
+/*
+realistic time-based goal
+big project in mind
+research related technologies
+*/
+
+const measureKelvin = function(){
+    const measurement = {
+        type: 'temp',
+        unit: 'celsius',
+        value: Number(prompt('Degrees celsius:')),
+    }
+
+    console.table(measurement)
+    const kelvin = measurement.value + 273
+    return kelvin;
+}
+
+console.log(measureKelvin())
