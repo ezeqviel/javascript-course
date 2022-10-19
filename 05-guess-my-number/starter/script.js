7ac494900f3bb0bd2ac1ee8f0812c9f992b58a35
eze <slgdslvsky@gmail.com>
2022-10-19 18:29:46 -0300

    selecting and manipulating elements

--- 05-guess-my-number/starter/script.js
+++ 05-guess-my-number/starter/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector('.message').textContent)
+
+document.querySelector('.message').textContent = 'Correct Number!'
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 912;
+
+console.log(document.querySelector('.score').value)
+document.querySelector('.guess').value = 912
