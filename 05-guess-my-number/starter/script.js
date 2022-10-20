92042d7f5f97d2138ae86789a895f97fc1d310de
eze <slgdslvsky@gmail.com>
2022-10-20 15:55:16 -0300

    handling click events

--- 05-guess-my-number/starter/script.js
+++ 05-guess-my-number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent)
 
 document.querySelector('.message').textContent = 'Correct Number!'
@@ -9,3 +9,13 @@ document.querySelector('.score').textContent = 912;
 
 console.log(document.querySelector('.score').value)
 document.querySelector('.guess').value = 912
+*/
+
+document.querySelector('.check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess)
+    
+    if (!guess) {
+        document.querySelector('.message').textContent = '⛔️ No number!'
+    }
+})
